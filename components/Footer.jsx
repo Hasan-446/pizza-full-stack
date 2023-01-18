d9bca7608bc22247c68377e106f8c05dbe76fea9
Hasan-446 <hasanmahmudul020@gmail.com>
2023-01-18 12:45:30 +0600

    first

---
+++ components/Footer.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID.THE BEST PIZZA BITE, WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            1654 R. Don Road #304.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className={styles.text}>
+            2356 K. Laquie Rd #235.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1011
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.logo}>
+          <Image src="/img/pizzaBitelogo.png" alt="" width="120" height="120" />
+        </div>
+       
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
