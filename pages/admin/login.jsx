0270b4b66176f7e2e96e4c340f126f7dddcfbfc8
Hasan-446 <hasanmahmudul020@gmail.com>
2023-01-30 02:14:14 +0600

    login functionality

---
+++ pages/admin/login.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import styles from "../../styles/Login.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState(false);
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1>Admin Dashboard</h1>
+        <input
+          placeholder="username"
+          className={styles.input}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          className={styles.input}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className={styles.button}>
+          Sign In
+        </button>
+        {error && <span className={styles.error}>Wrong Credentials!</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
