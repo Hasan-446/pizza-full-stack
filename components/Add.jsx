3b71578ffc88b5702f382ffdfdc2dc66013fa0e3
Hasan-446 <hasanmahmudul020@gmail.com>
2023-02-06 15:12:31 +0600

    updating

---
+++ components/Add.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../styles/Add.module.css"
+import axios from "axios";
+  
+
+const Add = ({setClose}) => {
+  return (
+    <div>Add</div>
+  )
+}
+
+export default Add
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from "next/head";
-import axios from "axios"
+import { useState } from "react";
+import axios from "axios";
 import Image from "next/image";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
+import AddButton from "../components/AddButton";
+import Add from "../components/Add";
 
-export default function Home({pizzaList}) {
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,17 +18,29 @@ export default function Home({pizzaList}) {
         <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Featured/>
-      <PizzaList pizzaList={pizzaList}/>
+      <Featured />
+      {admin && <AddButton setClose={setClose} />}
+      <PizzaList pizzaList={pizzaList} />
+      {!close && <Add setClose={setClose} />}
     </div>
   );
 }
 
-export const getServerSideProps = async()=>{
-  const res = await axios.get("http://localhost:3000/api/products",{headers:{"Content-Type" : "application/json"}});
-  return{
-    props:{
-      pizzaList: res.data,
-    }
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
   }
-}
+
+  const res = await axios.get("http://localhost:3000/api/products", {
+    headers: { "Content-Type": "application/json" },
+  });
+  return {
+    props: {
+      pizzaList: res.data,
+      admin,
+    },
+  };
+};
